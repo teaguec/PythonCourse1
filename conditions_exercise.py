686998b70acef9f888745839a173b42ef3ec4d7c
teaguec <caolan.teague@outlook.com>
2026-03-10 20:44:20 +0000

    Remembered sets are faster than lists, optimised function further

--- conditions_exercise.py
+++ conditions_exercise.py
@@ -1,18 +1,16 @@
-long_month = ['jan', 'mar', 'may', 'jul', 'aug', 'oct', 'dec']
-mid_month = ['apr', 'jun', 'sep', 'nov']
-
 
 def num_days(month):
 
-    if month in long_month:
-        print('number of days in',month,'is',31)
-    elif month in mid_month:
-        print('number of days in',month,'is',30)
+    if month in {'jan', 'mar', 'may', 'jul', 'aug', 'oct', 'dec'}:
+        days = 31
+    elif month in {'apr', 'jun', 'sep', 'nov'}:
+        days = 30
     elif month == 'feb':
-        print('number of days in',month,'is',28)
+        days = 28
     else:
         print('invalid month')
 
+    print('number of days in',month,'is',days)
 
 num_days(input("input month"))
 # optimize/shorten the code in the function
